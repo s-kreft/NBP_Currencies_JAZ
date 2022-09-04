2c95dd4bef305e75dc6a01f70b24d5680a0b4313
s-kreft <sebastian.kreft.sg@gmail.com>
2022-09-04 14:08:45 +0200

    first commit

---
+++ src/test/java/com/example/jaz_poprawa/JazPoprawaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jaz_poprawa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JazPoprawaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
